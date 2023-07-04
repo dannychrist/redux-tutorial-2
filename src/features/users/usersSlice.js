716fe91ec8eca8212aab71a5cced741cabe96381
dannychrist <dannyjameschrist@gmail.com>
2023-07-04 12:52:36 -0400

    new changes

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [
+  {
+    id: '0',
+    name: 'Dude Lebowski',
+  },
+  { id: '1', name: 'Neil Young' },
+  { id: '2', name: 'Bandy DeVito' },
+];
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+
+    
+  },
+});
+
+export const selectAllUsers = (state) => state.users;
+
+export default usersSlice.reducer;
