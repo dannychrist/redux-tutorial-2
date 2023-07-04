ed27215a0bcba00f74f21dc5665a2f06a23817ba
dannychrist <dannyjameschrist@gmail.com>
2023-07-04 11:15:55 -0400

    new changes

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,19 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-
-}
+const initialState = [
+  {
+    id: '1',
+    title: 'Learning Redux Toolkit',
+    content: "I've heard good things",
+  },
+  {
+    id: '2',
+    title: 'Slices...',
+    content: 'The more I slice, the more I want pizza.',
+  },
+];
 
 export const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {
-
-  }
+  reducers: {},
 });
 
 export const {
-
+  
 } = postsSlice.actions;
 
 export default postsSlice.reducer;
