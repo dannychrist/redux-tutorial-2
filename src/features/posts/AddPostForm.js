ad1782d34f92782f76afc87ff41bde02b14c157a
dannychrist <dannyjameschrist@gmail.com>
2023-07-04 13:02:53 -0400

    new chages

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,23 +1,33 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { nanoid } from '@reduxjs/toolkit';
+import { useDispatch, useSelector } from 'react-redux';
 import { postAdded } from './postsSlice';
+import { selectAllUsers } from '../users/usersSlice';
 
 const AddPostForm = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [userId, setUserId] = useState('');
+
+  const users = useSelector(selectAllUsers);
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
 
   function onSavePostClicked() {
     if (title && content) {
-      dispatch(postAdded(title, content));
+      dispatch(postAdded(title, content, userId));
       setTitle('');
       setContent('');
     }
   }
+
+  const usersOptions = users.map((user, index) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
     <section>
@@ -31,11 +41,11 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
-        {/* <label htmlFor='postAuthor'>Author:</label>
-        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
-                    <option value=""></option>
-                    {usersOptions}
-                </select> */}
+        <label htmlFor='postAuthor'>Author:</label>
+        <select id='postAuthor' value={userId} onChange={onAuthorChanged}>
+          <option value=''></option>
+          {usersOptions}
+        </select>
         <label htmlFor='postContent'>Content:</label>
         <textarea
           id='postContent'
