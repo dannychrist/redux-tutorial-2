c5c14cd21e39d3b061495341719234ddfa033252
dannychrist <dannyjameschrist@gmail.com>
2023-07-05 09:48:46 -0400

    new changes

--- src/features/posts/PostsExcerpt.js
+++ src/features/posts/PostsExcerpt.js
@@ -10,7 +10,7 @@ const PostsExcerpt = ({ post }) => {
       <h3>{post.title}</h3>
       <p className='excerpt'>{post.body.substring(0, 75)}</p>
       <p className='postCredit'>
-        <Link to={`post/${post.id}`}></Link>
+        <Link to={`post/${post.id}`}>View Post</Link>
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
